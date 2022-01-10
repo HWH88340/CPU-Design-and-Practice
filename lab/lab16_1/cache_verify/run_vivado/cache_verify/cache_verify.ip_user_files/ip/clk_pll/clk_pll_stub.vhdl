b1d37ddcede17e5a512347b6e3b6cfb761f2d9fd
HWH88340 <hwh88340@gmail.com>
2022-01-10 18:56:48 +0800

    cache

---
+++ lab/lab16_1/cache_verify/run_vivado/cache_verify/cache_verify.ip_user_files/ip/clk_pll/clk_pll_stub.vhdl
@@ -0,0 +1,29 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+-- Date        : Mon Jan 10 18:55:29 2022
+-- Host        : LAPTOP-NDLNQHR8 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/33381/CPU-Design-and-Practice/lab/lab16_1/cache_verify/rtl/xilinx_ip/clk_pll_stub.vhdl
+-- Design      : clk_pll
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a200tfbg676-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_pll is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_pll;
+
+architecture stub of clk_pll is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,clk_in1";
+begin
+end;
